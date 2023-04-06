118f01609bdafe040ac2b22af71f60770d24a4da
Eun Deok, Lee <pleed0215@gmail.com>
2023-04-06 18:43:44 +0900

    26.6 finished

---
+++ functions/src/index.ts
@@ -0,0 +1,48 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+admin.initializeApp();
+export const onVideoCreated = functions.firestore
+  .document("videos/{videoId}")
+  .onCreate(async (snapshot, context) => {
+    await snapshot.ref.update({ hello: "from functions" });
+    const spawn = require("child-process-promise").spawn;
+    const video = snapshot.data();
+    const destPath = `/tmp/${snapshot.id}.jpg`;
+    await spawn("ffmpeg", [
+      "-i",
+      video.fileUrl,
+      "-ss",
+      "00:00:01.000",
+      "-vframes",
+      "1",
+      "-vf",
+      "scale=150:-1",
+      destPath,
+    ]);
+    const storage = admin.storage();
+    const [uploaded, _] = await storage.bucket().upload(destPath, {
+      destination: `thumbnails/${snapshot.id}.jpg`,
+    });
+
+    await uploaded.makePublic();
+    await snapshot.ref.update({
+      thumbnailUrl: uploaded.publicUrl(),
+    });
+
+    const db = admin.firestore();
+    await db
+      .collection("users")
+      .doc(video.creatorUid)
+      .collection("videos")
+      .doc(snapshot.id)
+      .set({ thumbnailUrl: uploaded.publicUrl(), videoId: snapshot.id });
+  });
